ddd7f1c6f4ab1b5ca7325644b1d0592130d131fb
DDataAly <aly.3126d@slmail.me>
2024-12-13 12:41:47 +0000

    Completed project

--- 03_birthday_paradox/birthday_match.py
+++ 03_birthday_paradox/birthday_match.py
@@ -1,13 +1,20 @@
 import random
-# Describe the task
+# Credentials
 def intro():
-    print('''Birthday Paradox Simulation, from Al Sweigart's Big Book of Small Projects.
-See readme.md for the task details. Code is written by me.''')
+    print('''\nBirthday Paradox Simulation, from Al Sweigart's Big Book of Small Projects. The birthday paradox
+shows us that in a group of N people, the odds that two of them have matching birthdays is surprisingly large. 
+This program does a Monte Carlo simulation to explore this concept. Code is written by me.''')
     
 # Ask user to input the parameters of the probability calculation
 def experiment_parameters():
-    n_experiments=int(input('Please enter the number of experiments: '))
-    n_ppl=int(input('Please enter the number of ppl in the group (up to 100 ppl): '))
+    n_experiments=int(input('\nPlease enter the number of experiments: '))
+    valid_input=False
+    while valid_input==False:
+        n_ppl=int(input('Please enter the number of ppl in the group (up to 100 ppl): '))
+        if n_ppl>0 and n_ppl<100:
+            valid_input=True
+        else:
+            print('Please enter a valid size of the group. ')    
     return(n_experiments,n_ppl)
 
 # Generate random month of birth
@@ -34,48 +41,62 @@ def bd_date(month):
     else:
         date=bd_date_feb_leap()
     return(date)
-    
-# Check if there is at least one duplicating birthday in the group of given size 
-def duplicates_checker(n_ppl):    
+
+# Display birthdays in text format, separating them with commas
+def birthday_display(n_ppl):
     group_birthdays=[]
+    months=('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec')
     for person in range(1, n_ppl+1):
         p_month=bd_month()
-        p_date=bd_date(p_month)
-        dm=(p_date,p_month)
+        p_date=bd_date(p_month)     
+        for index,month in enumerate(months):
+            if p_month==index+1:
+                dm=(str(p_date)+' '+month)
         group_birthdays.append(dm)
+    for index,birthday in enumerate(group_birthdays):
+        if index!=0:
+            print(', '+birthday, end =' ')
+        else:
+            print(birthday, end =' ')
+    return(group_birthdays)
+
+
+# Check if there is at least one duplicating birthday in the group of given size 
+def duplicates_checker(group_birthdays):    
     unique_group_birthdays=set(group_birthdays)
+    duplicates=set([birthday for birthday in unique_group_birthdays if group_birthdays.count(birthday)>1])
     if len(unique_group_birthdays)!=len(group_birthdays):
+        print(f'\nIn this simulation, multiple people have birthdays on: ')
+        for birthday in duplicates:
+            print(birthday, ' ')
         return(True)
     else:
+        print(f'\nIn this simulation, there are no matching birthdays in the group. ')
         return(False)
 
 
-intro()
 # Calculating probability of two or more people in the group having birthday at the same day
+intro()
 num_experiments, group_size = experiment_parameters()
 total_experiments=num_experiments
 match=0
 notification_counter=1
+print('Ready to start simulations.')
+input('Press enter to begin...')
 while num_experiments>0:
-    m=duplicates_checker(group_size)
+    print('\nRunning the experiment...')
+    group_birthdays=birthday_display(group_size)
+    m=duplicates_checker(group_birthdays)
     if m==True:
         match+=1
     num_experiments-=1 
-    if (total_experiments - num_experiments)%100==0:
+    if (total_experiments - num_experiments)%1_00==0 and (total_experiments!=(total_experiments-num_experiments)):
         print(f'We run {notification_counter*100} experiments. Work in progress...')
         notification_counter+=1
 
-print(f'''We run {total_experiments} experiments for a group of {group_size} people.
+print(f'''\nWe run {total_experiments} simulations for a group of {group_size} people.
 There were {match} cases where at least two people in the group had a birthday at the same day.      
 The probability of at least two people having birthday at the same day in this group is {(match/total_experiments):.2%}''')    
-
- 
-    
-
-
-
-
-
 
 
 
