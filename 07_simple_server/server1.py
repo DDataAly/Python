b2f13211314ebabaf708e49b3bc65fd9667d8bd7
DDataAly <alyona.dorof@gmail.com>
2025-02-28 19:48:15 +0000

    Extended the functionality of the do_GET method

--- 07_simple_server/server1.py
+++ 07_simple_server/server1.py
@@ -1,38 +1,63 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib.parse import urlparse, parse_qs
 
-class myHandler(BaseHTTPRequestHandler):
+# Telling server what to do when receiving a request for the client
+class MyHandler(BaseHTTPRequestHandler):
     storage = {}
 
-    def do_GET(self):
-        print(f"this is the path {self.path}")
+    # This block of code tells server what to send to the client
+    def send_response_message (self,return_object):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write(return_object.encode())
+
+
+    def do_GET(self):   
+
+        # This block of code tells how to split the clients URL to retrieve the information we need (key-value pair)
+        print(f"this is the path {self.path}")    
         url = self.path
         parsed_url = urlparse (url)
         url_query_dict=parse_qs(parsed_url.query)
 
-
+        # This block of code handles set requests (adds new key-value pairs to the storage)
+        # Using next(iter) to access the first (and only) key-pair in the dictionary since dictionary.items() returns a view object
         if self.path.startswith('/set'):
-            new_storage_item={k:v[0] for k,v in url_query_dict.items()}
-            print(f'This is new pair to store {new_storage_item}')
-            self.storage.update(new_storage_item)
-            print(f'This is what the storage contains: {self.storage}')
+            new_storage_item = {k:v[0] for k,v in url_query_dict.items()}
+            new_storage_item_key, new_storage_item_value = next(iter(new_storage_item.items()))
 
-            self.send_response(200)
-            self.send_header('Content-type', 'text/plain')
-            self.end_headers()
-            self.wfile.write(f'The pair {new_storage_item} was added to the storage'.encode())
+            if new_storage_item_key in self.storage and new_storage_item_value in self.storage.values():
+                return_object = f'The pair {new_storage_item} is already in the storage'
+
+            elif new_storage_item_key in self.storage:
+                return_object = f'This key is already used. Please change the key to add new value to the storage'
+            
+            else:      
+                self.storage.update(new_storage_item)
+                return_object = f'The pair {new_storage_item} was added to the storage'
 
 
-        if self.path.startswith('/get'):
-            item_to_fetch_key = url_query_dict['key'][0]
-            item_to_fetch_value = self.storage[item_to_fetch_key]  
+        # This block of code handles get requests (retrieves the requested value from the storage)
+        elif self.path.startswith('/get'):
+            # Using dictionary.get method to access the value of the key Or return None if value doesn't exist or the word 'key' is missing
+            item_to_fetch_key = url_query_dict.get('key', None)[0]
+            if item_to_fetch_key != None:
+                item_to_fetch_value = self.storage[item_to_fetch_key] 
+                return_object = item_to_fetch_value                          
+            else:
+                return_object = f'The key is not in the storage or the request doesn\'t contain the "key" word'
 
-            self.send_response(200)
-            self.send_header('Content-type', 'text/plain')
-            self.end_headers()
-            self.wfile.write(item_to_fetch_value.encode())
+        else:
+            return_object = f'Server can\'t process this request'
+        
+        # This block sends the reply to the client
+        self.send_response_message (return_object)
 
-with HTTPServer(('127.0.0.1', 4000), myHandler) as server:
+
+# This block of code runs a server on port 400 of the localhost
+# With block makes sure that server stops running on when we close the program
+with HTTPServer(('127.0.0.1', 4000), MyHandler) as server:
     server.serve_forever()     
 
 
