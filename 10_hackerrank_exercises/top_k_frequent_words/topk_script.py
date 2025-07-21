7a47f327f5b1ce9c762121081293197d230f8bf1
DDataAly <alyona.dorof@gmail.com>
2025-07-21 18:21:16 +0100

    Adds run block in the script

--- 10_hackerrank_exercises/top_k_frequent_words/topk_script.py
+++ 10_hackerrank_exercises/top_k_frequent_words/topk_script.py
@@ -15,18 +15,25 @@ def find_k_most_freq_words (swapped: list, k:int) -> list:
     return heapq.nsmallest(k,swapped)
 
 
-def output_top_k_most_freq_words(given_list:list, k:int) ->list:
+def list_k_most_freq_words(given_list:list, k:int) ->list:
     freq_dict = count_words_freq(given_list)
     swapped = swap_order(freq_dict)
     k_most_freq_words = find_k_most_freq_words(swapped,k)
     return [word for _, word in k_most_freq_words]
     
 
-def print_individual_words(most_freq_words:list):
+def print_individual_words(most_freq_words:list,k:int):
+    if len(most_freq_words) < k:
+        print (f'Number of unique strings in the given list is less than k = {k}')
+    print('Most frequent words are: ')     
     for word in most_freq_words:
         print (word)
 
-
+if __name__=='__main__':
+    k = 3
+    given_list = [10, 9, 'cat', 'cat', 'dog', 'dog']
+    most_freq_words = list_k_most_freq_words(given_list, k)
+    print_individual_words(most_freq_words,k)
 
 
 
