03d549f7bf91fdaba4d7025d0555a3c3f44c70b0
DDataAly <alyona.dorof@gmail.com>
2025-07-25 22:08:44 +0100

    Solves the task

--- 10_hackerrank_exercises/dynamic_median/median.py
+++ 10_hackerrank_exercises/dynamic_median/median.py
@@ -1,51 +1,41 @@
 import heapq
-import sys
 
 class MedianFinder:
-    def __init__(self):  # Don't pass any variables, so self only
-        self.left_heap = [] # Each object has attributes which are created by the system not passed from the outside
-        self.right_heap = [] # They are always empty lists, so no user input here
+    def __init__(self):  
+        self.left_heap = [] 
+        self.right_heap = [] 
         
 
     def add_num(self, num: int) -> None:
-        heapq.heapify(self.left_heap)
-        heapq.heapify(self.right_heap)
-
-        if len(self.left_heap) == 0:
-            self.left_heap.append(-num)
-            heapq.heapify(self.left_heap)
-        elif - num > self.left_heap[0]:
-            self.left_heap.append(-num)
+        if len(self.left_heap) == 0 or - num > self.left_heap[0]:
+            heapq.heappush(self.left_heap, -num)
         else: 
-            self.right_heap.append(num)    
+            heapq.heappush(self.right_heap, num)  
         
-        if len(self.left_heap) <3 and len(self.right_heap) == 0:
-            pass
-        elif len(self.left_heap) > len(self.right_heap) + 1:
-            self.right_heap.append(-heapq.heappop(self.left_heap))
+        if len(self.left_heap) > len(self.right_heap) + 1:
+            heapq.heappush(self.right_heap, (-heapq.heappop(self.left_heap)))
         elif len(self.right_heap) > len(self.left_heap):
-            self.left_heap.append(heapq.heappop(self.right_heap))    
+            heapq.heappush(self.left_heap, (-heapq.heappop(self.right_heap)))
 
 
     def find_median(self) -> float:
         if len(self.right_heap) == len(self.left_heap):
-            median = round((float(((-heapq.heappop(self.left_heap)) + heapq.heappop(self.right_heap))/2)),5)
+            median = ((-self.left_heap[0]) + (self.right_heap[0]))/2
         else:
-            median = (-heapq.heappop(self.left_heap))
+            median = (-self.left_heap[0])
         return median
     
+
 if __name__ == "__main__":    
     m = MedianFinder()
     m.add_num(2)
     m.add_num(3)
     m.add_num(4)
-
-
-
+    m.add_num(5)
     print(m.find_median())
 
 
-
+# Hackerrank running block
 # if __name__ == "__main__":
 #     n = int(input())
 #     mf = MedianFinder()
@@ -55,3 +45,7 @@ if __name__ == "__main__":
 #             mf.add_num(int(line[1]))
 #         else:
 #             print(f"{mf.find_median():.1f}")
+
+
+
+
