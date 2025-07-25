e27e22550ffdf5b79e216ec875b1b732ba7f8a34
DDataAly <alyona.dorof@gmail.com>
2025-07-25 11:18:44 +0100

    Adds alternative solution using pandas

--- 10_hackerrank_exercises/top_k_frequent_words/topk_script.py
+++ 10_hackerrank_exercises/top_k_frequent_words/topk_script.py
@@ -1,5 +1,8 @@
 import collections
-import heapq
+import heapq 
+import pandas as pd
+
+# Option 1 - Heap solution
 
 def count_words_freq(given_list: list) -> dict:
     str_given_list = [item for item in given_list if isinstance(item, str)]
@@ -29,11 +32,29 @@ def print_individual_words(most_freq_words:list,k:int):
     for word in most_freq_words:
         print (word)
 
+#Option 2 - Pandas solution
+
+def count_words_freq(given_list: list) -> dict:
+    str_given_list = [item for item in given_list if isinstance(item, str)]
+    freq_dict = collections.Counter(str_given_list)  
+    return freq_dict
+
+
+def get_top_k(freq_dict, k):
+    dict_for_df = {'word': list(freq_dict.keys()),'frequency':list(freq_dict.values())}
+    freq_df = pd.DataFrame(dict_for_df)
+    freq_df.sort_values(by =['frequency', 'word'], ascending=[False, True], inplace =  True)
+    top_k_words = freq_df['word'].head(k+1).tolist()
+    return top_k_words
+
+
 if __name__=='__main__':
     k = 3
-    given_list = [10, 9, 'cat', 'cat', 'dog', 'dog']
-    most_freq_words = list_k_most_freq_words(given_list, k)
-    print_individual_words(most_freq_words,k)
-
+    given_list = [10, 9, 'cat', 'cat', 'dog', 'dog','i','i','i', 'rose','rose','rose']
+    freq_dict = count_words_freq(given_list)
+    top_k_words = list_k_most_freq_words(given_list, k)  #Option 1 -comment out for Opt 2
+    top_k_words = get_top_k(freq_dict,2)  #Option 2 -comment out for Opt 1
+    
+    print_individual_words(top_k_words,k)
 
 
